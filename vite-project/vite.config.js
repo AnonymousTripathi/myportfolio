c431b8d4a5f4bf30dfdfeeaacd972908f0f7e7eb
Harsh <tripathiharsh227@gmail.com>
2025-04-10 11:28:10 +0530

    7

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  root:'.',
   plugins: [react(),tailwindcss()],
+  build:{
+    outDir:'../dist',
+  }
 })
