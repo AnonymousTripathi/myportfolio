5d3d8229e16e86ea8f40bc31032eae064c4efec1
Harsh <tripathiharsh227@gmail.com>
2025-07-22 10:21:15 +0530

    21

--- vite-project/src/constants/index.js
+++ vite-project/src/constants/index.js
@@ -2,6 +2,7 @@
 import twitter from "../assets/projects/twitter.png"
 import zeroth from "../assets/projects/zeroth.png"
 import portfolio from "../assets/projects/portfolio.png"
+import ecom  from "../assets/projects/ecom.png"
 export const HERO_CONTENT = `I am a Full Stack Developer eager to leverage a strong skillset in the MERN stack (MongoDB, Express.js, React.js, Node.js). Proven ability to learn quickly and apply theoretical knowledge to practical scenarios. Seeking an opportunity to contribute to challenging projects and further develop expertise in building scalable and efficient web applications.`;
 
 // export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
@@ -50,11 +51,11 @@ export const PROJECTS = [
   }, 
   {
     title: "Full Stack E-commerce Website with AI Product Description",
-    image: portfolio,
+    image: ecom,
     description:
       "A complete MERN-based e-commerce platform with integrated AI-powered product description generation using Cohere API. Features include filtering, size selection, cart system, Razorpay payments, order tracking, and a secure admin dashboard for product, user, and order management.",
     technologies: ["Express.js", "React.js", "Node.js", "MongoDB","Tailwind CSS","Cohere API"],
-    link:"/"
+    link:"https://ecom-frontend-m4gh.onrender.com"
   }, 
 ];
 
